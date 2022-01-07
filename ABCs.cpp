8e2e520137945b64ce530a624ff128de68ef7769
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2022-01-07 11:40:19 -0800

    Create ABCs.cpp

---
+++ ABCs.cpp
@@ -0,0 +1,12 @@
+// Given three integers AA, BB, and CC, determine their sum.
+// Your task is to implement the function getSum(A, B, C),
+// which returns the sum A + B + CA+B+C.
+
+#include <bits/stdc++.h>
+using namespace std;
+// Write any include statements here
+
+int getSum(int A, int B, int C) {
+  // Write your code here
+  return A+B+C;
+}
